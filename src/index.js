2fdbb8d1cb145552dba000ec7a891d4d421d2a78
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-11-10 11:55:45 +0100

    cycle colors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
 const logo = require('../static/logo.svg');
 
-function start(img1, img2) {
+function start(images) {
+  var duration = 500;
   var im = document.getElementById('target');
   im.width = 640;
   
   function animate(t) {
-    if (t % 1000 > 500) {
-      im.src = img1;
-    } else {
-      im.src = img2;
-    }
+    var index = Math.floor(t % (duration * images.length) / duration);
+    im.src = images[index];
     requestAnimationFrame(animate);
   }
   requestAnimationFrame(animate);
 }
-
 
 var img = new Image();
 img.onload = function() {
   var canvas = document.getElementById('work');
   var ctx = canvas.getContext('2d');
 
-  var w = 64;
-  var h = w * img.height / img.width;
-  canvas.width = w;
-  canvas.height = h;
-  ctx.drawImage(img, 0, 0, w, h);
-  var pixelized = canvas.toDataURL();
+  widths = [63];
+  colors = ['#e1706e', '#f2cfc9', '#4a8daf', '#d8d8d8', '#6ec3d0', '#1c2b49]'];
+
+  images = [logo];
+  for (var i = 0; i < widths.length; i++) {
+    var w = widths[i];
+    var h = w * img.height / img.width;
+    for (var j = 0; j < colors.length; j++) {
+      canvas.width = w;
+      canvas.height = h;
+      ctx.clearRect(0, 0, w, h);
+      ctx.drawImage(img, 0, 0, w, h);
+      ctx.globalCompositeOperation = 'source-in';
+      ctx.fillStyle = colors[j];
+      ctx.fillRect(0, 0, w, h);
+      images.push(canvas.toDataURL());
+    }
+  }
   canvas.parentElement.removeChild(canvas);
   canvas = undefined;
-  start(logo, pixelized);
+  start(images);
 }
 
 img.src = logo;
